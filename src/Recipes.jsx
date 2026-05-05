9c9aa0fc247e8beed37361c431a1d6467962fec5
Ivelina <kristinevans621@gmail.com>
2026-05-05 23:41:26 +0300

    Fetch recipes from json-server API

--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,30 +1,15 @@
+import { useEffect, useState } from "react";
+
 function Recipes() {
-    const recipes = [
-        {
-            id: 1,
-            title: "Pasta Carbonara",
-            description: "A simple Italian pasta recipe with eggs, cheese and bacon.",
-            image: "https://home-design.bg/wp-content/uploads/2023/05/klasicheska-recepta-za-spageti-karbonara-4.webp"
-        },
-        {
-            id: 2,
-            title: "Pancakes",
-            description: "Soft homemade pancakes, perfect for breakfast.",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo-wpWoWqcDjKP7fR6B5jhKyflCkd__7EaMQ&s"
-        },
-        {
-            id: 3,
-            title: "Greek Salad",
-            description: "Fresh salad with tomatoes, cucumber, olives and feta cheese.",
-            image: "https://trapezata.net/media/k2/items/cache/5a61d31ed794cb758475f6c89477dfed_XL.jpg"
-        },
-        {
-            id: 4,
-            title: "Lasagna",
-            description: "Classic Italian lasagna with layers of pasta, meat sauce and cheese.",
-            image: "https://recipe-graphics.grocerywebsite.com/0_GraphicsRecipes/1391_4k.jpg"
-        }
-    ];
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/recipes")
+            .then((response) => response.json())
+            .then((data) => setRecipes(data))
+            .catch((error) => console.log(error));
+    }, []);
 
     return (
         <div className="page">
